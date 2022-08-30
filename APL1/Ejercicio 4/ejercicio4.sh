761ff7fc2bde375240531c5aacc72e9a1dbdf2af
ssgalo <santigalo32@gmail.com>
2022-08-30 01:54:01 -0300

    Resolucion parcialmente terminada - EJ4

--- APL1/Ejercicio 4/ejercicio4.sh
+++ APL1/Ejercicio 4/ejercicio4.sh
@@ -22,16 +22,120 @@
 
 ayuda() {
 	echo "-------------- AYUDA - DESCRIPCIÓN DEL SCRIPT --------------"
-	echo "Dados archivos de log donde se registraron todas las llamadas realizadas en una semana por un call center, el script obtiene y muestra por pantalla los siguientes datos:"
-	echo "1. Promedio de tiempo de las llamadas realizadas por día"
-	echo "2. Promedio de tiempo y cantidad por usuario por día"
-	echo "3. Los 3 usuarios con más llamadas en la semana"
-	echo "4. Cuántas llamadas no superan la media de tiempo por día"
-	echo "5. El usuario que tiene más cantidad de llamadas por debajo de la media en la semana"
+	echo "Este es un script que cuenta la cantidad de lineas de codigo y de comentarios que poseen los archivos en la ruta pasada"
+	echo "Solo analiza los archivos con las extensiones pasadas por parametro, y lo hace recursivamente para cada subdirectorio"
 	echo "---------------- AYUDA - FORMATO DEL SCRIPT ----------------"
 	echo "./ejercicio4.sh [-h / -? / --help]: Muestra la ayuda"
 	echo "./ejercicio4.sh [--ruta <ubicacion_directorio> --ext <extension1,extension2,extensionN>]: Realiza el analisis en el directorio cuya ubicación es ubicacion_directorio para las extensiones especificadas luego de --ext"
 	return 0
+}
+
+directorioNoValido(){
+	echo "Error. Directorio no valido"
+	ayuda
+	exit 1
+}
+
+calcularSalida(){
+
+	cantArchivos=0
+	if [ ! -d "$directorio" ] ; then
+		directorioNoValido "$directorio"
+	fi
+	
+	for i in ${!extensiones[*]}
+	do
+		
+		rutaPadre=$(find "$directorio" -name "*."${extensiones[i]}"") 
+
+		#for arch in "${directorio}"**/*."${extensiones[i]}" 
+		
+		for arch in $rutaPadre 
+		do
+			if [ -f "$arch" ]
+			then
+				(( cantArchivos++ ))
+			comentarios=0
+			comentariosMultilinea=0
+			codigos=0	
+				
+			echo El archivo analizado es: $arch
+			
+			#Se analizan los comentarios del tipo /* */ (incluyendo multilinea)
+			#Lo unico que no se tiene en cuenta son los del tipo /* here is your comment */ code :( (no saliò)
+			
+			(( comentariosMultilinea+=$(awk '
+			BEGIN{ 
+			comentarios=0 
+			}
+			/\/\*/,/\*\//{
+			++comentarios 
+			}
+			END{
+			print comentarios
+			}' $arch) ))
+			
+			
+			#A continuaciòn se analizan los comentarios de una sola linea, //
+			#Tambièn incluye el analisis de code //here is your comment :)
+		
+			#respuestas es una variable que representa la salida del awk
+			#luego, a travès del comando read se pasa a un array (validas)
+			#(optimizar!!!)
+			
+			respuestas=$(cat $arch | awk '
+			BEGIN{
+			FS="//"
+			comentarios=0
+			codigos=0
+			}
+			{
+			
+			if(NF == 1){
+				gsub("[[:blank:]]", "", $1)
+				
+				if(length($1) > 0)	
+					codigos++
+			}
+			if(NF > 1){
+				gsub("[[:blank:]]", "", $1)
+				
+				if(length($1) > 0){
+					codigos++
+					comentarios++
+				} else {
+					comentarios++
+				}
+			}
+			}
+			END{
+				print codigos,comentarios
+			}
+			' )
+			
+			read -ra validas <<< "$respuestas"
+			
+			#A codigos se le resta los comentarios multilinea porque en el segundo analisis los contarìamos como còdigo
+			
+			((codigos+=validas[0]))
+			((comentarios+=validas[1]))
+			((comentarios+=comentariosMultilinea))
+			((codigos-=comentariosMultilinea))
+			echo Cantidad de lineas de codigo: $codigos 
+			echo Cantidad de comentarios: $comentarios
+			
+			echo 
+			fi
+		done		
+	done
+	
+	if [ $cantArchivos -gt 0 ]
+	then
+		echo Archivos totales analizados: $cantArchivos
+	else
+		echo "No hay archivos para analizar en la ruta pasada con esas extensiones."
+	fi
+	
 }
 
 validarDirectorio() {
@@ -60,7 +164,8 @@ fi
 if [[ "$1" == "--help" ||  "$1" == "-h" || "$1" == "--?" ]]
 then
 	ayuda
-	exit
+else
+	calcularSalida
 fi
 
 # ------------------------ FIN DE ARCHIVO ------------------------
